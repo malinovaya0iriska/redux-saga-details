e093e7147fb1a8d564a9e88124885b1bfd3e6881
Irina Martsulevich <malinovayairiska@gmail.com>
2022-07-22 12:47:41 +0300

    add effects takeLatest, takeEvery, takeLeading

--- src/store/sagas/index.ts
+++ src/store/sagas/index.ts
@@ -1,16 +1,18 @@
-import { take } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga/effects';
 
+const wait = (delay: number) => new Promise((res) => setTimeout(res, delay));
 // business logic
 export function* workerSaga() {
+  yield wait(1000);
   console.log('click from saga');
 }
 
 // actions
 export function* watchClickSaga() {
-  while (true) {
-    yield take('CLICK');
-    yield workerSaga();
-  }
+  // yield takeEvery('CLICK', workerSaga);
+
+  yield takeLatest('CLICK', workerSaga);
+  // yield takeLeading('CLICK', workerSaga);
 }
 
 export default function* rootSaga() {
