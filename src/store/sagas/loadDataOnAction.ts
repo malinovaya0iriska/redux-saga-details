58bb3caa92d56b3d82af3c2b7583c3902d31cb8e
Irina Martsulevich <malinovayairiska@gmail.com>
2022-07-28 14:15:33 +0300

    feat: add variant for execution effects in channel all on turn

--- src/store/sagas/loadDataOnAction.ts
+++ src/store/sagas/loadDataOnAction.ts
@@ -1,33 +1,27 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { apply, call, cancel, fork, put, take } from 'redux-saga/effects';
+import { actionChannel, apply, call, put, take } from 'redux-saga/effects';
 import { BASE_URL } from '../../api';
 
-// pattern take + fork - the most common pattern for React + Saga
-// fork creates non-blocking requests, parallel tasks
+// pattern take + call + actionChannel
+// collect all actions and execute them one by one
 
 export default function* loadDataOnAction() {
-  let task;
-  let abortController = new AbortController();
+  // @ts-ignore
+  const channel = yield actionChannel('LOAD_DATA');
   while (true) {
     // @ts-ignore
-    yield take('LOAD_DATA');
+    yield take(channel);
 
-    if (task) {
-      abortController.abort();
-      yield cancel(task);
-
-      abortController = new AbortController();
-    }
     // @ts-ignore
-    task = yield fork(fetchDataOnAction, abortController.signal);
+    yield call(fetchDataOnAction);
   }
 }
 
-export function* fetchDataOnAction(signal: any) {
+export function* fetchDataOnAction() {
   console.log('Fetching started .... ');
 
   // @ts-ignore
-  const response = yield call(fetch, `${BASE_URL}/planets`, { signal });
+  const response = yield call(fetch, `${BASE_URL}/planets`);
   // @ts-ignore
   const data = yield apply(response, response.json);
   console.log('PLANETS', data);
