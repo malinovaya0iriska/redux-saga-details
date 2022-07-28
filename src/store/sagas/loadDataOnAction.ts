9e26d9821a9d67cfd103ce01cc88b28af35a619c
Irina Martsulevich <malinovayairiska@gmail.com>
2022-07-28 13:58:01 +0300

    feat: add: loading data on action without blocking requests

--- src/store/sagas/loadDataOnAction.ts
+++ src/store/sagas/loadDataOnAction.ts
@@ -1,20 +1,27 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { apply, call, put, take } from 'redux-saga/effects';
+import { apply, call, fork, put, take } from 'redux-saga/effects';
 import { BASE_URL } from '../../api';
 
-// pattern take + call
-// call will block the saga, so it cannot catch next take till it will be finished
+// pattern take + fork - the most common pattern for React + Saga
+// fork creates non-blocking requests, parallel tasks
 
 export default function* loadDataOnAction() {
   while (true) {
     // @ts-ignore
     yield take('LOAD_DATA');
     // @ts-ignore
-    const response = yield call(fetch, `${BASE_URL}/planets`);
-    // @ts-ignore
-    const data = yield apply(response, response.json);
-    console.log('PLANETS', data);
-
-    yield put({ type: 'PLANETS_LOADED', payload: data });
+    yield fork(fetchDataOnAction);
   }
+}
+
+export function* fetchDataOnAction() {
+  console.log('Fetching started .... ');
+
+  // @ts-ignore
+  const response = yield call(fetch, `${BASE_URL}/planets`);
+  // @ts-ignore
+  const data = yield apply(response, response.json);
+  console.log('PLANETS', data);
+
+  yield put({ type: 'PLANETS_LOADED', payload: data });
 }
