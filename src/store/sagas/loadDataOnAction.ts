2bfa694c9494584aa5c00f308d6628ca4359fb41
Irina Martsulevich <malinovayairiska@gmail.com>
2022-07-28 14:07:09 +0300

    feat: add custom takeLast effect for cancelation of multiple actions

--- src/store/sagas/loadDataOnAction.ts
+++ src/store/sagas/loadDataOnAction.ts
@@ -1,24 +1,33 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { apply, call, fork, put, take } from 'redux-saga/effects';
+import { apply, call, cancel, fork, put, take } from 'redux-saga/effects';
 import { BASE_URL } from '../../api';
 
 // pattern take + fork - the most common pattern for React + Saga
 // fork creates non-blocking requests, parallel tasks
 
 export default function* loadDataOnAction() {
+  let task;
+  let abortController = new AbortController();
   while (true) {
     // @ts-ignore
     yield take('LOAD_DATA');
+
+    if (task) {
+      abortController.abort();
+      yield cancel(task);
+
+      abortController = new AbortController();
+    }
     // @ts-ignore
-    yield fork(fetchDataOnAction);
+    task = yield fork(fetchDataOnAction, abortController.signal);
   }
 }
 
-export function* fetchDataOnAction() {
+export function* fetchDataOnAction(signal: any) {
   console.log('Fetching started .... ');
 
   // @ts-ignore
-  const response = yield call(fetch, `${BASE_URL}/planets`);
+  const response = yield call(fetch, `${BASE_URL}/planets`, { signal });
   // @ts-ignore
   const data = yield apply(response, response.json);
   console.log('PLANETS', data);
